307ecd3e47bb44fada03d791c5b249c0656bc72b
devtrungk3 <ndtrung30@gmail.com>
2025-07-15 17:48:59 +0700

    add check exists by username query into UserRepository
    add some queries for get projects and statistics, optimize custom hpql

--- server/src/main/java/com/example/server/repositories/ProjectRepository.java
+++ server/src/main/java/com/example/server/repositories/ProjectRepository.java
@@ -1,27 +1,36 @@
 package com.example.server.repositories;
 
 import com.example.server.dto.ProjectDTO;
+import com.example.server.dto.StatusCountDTO;
 import com.example.server.models.Project;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
     @Query("""
-            SELECT new com.example.server.dto.ProjectDTO(p.id, p.name, p.status, o.username, p.createdAt, p.updatedAt)
+            SELECT new com.example.server.dto.ProjectDTO(p.id, p.name, p.status, p.owner.username, p.createdAt, p.updatedAt)
             FROM Project p
-            JOIN p.owner o
-            WHERE o.id = :ownerId
+            WHERE p.owner.id = :ownerId
+            ORDER BY p.updatedAt DESC, p.createdAt DESC
             """)
-    Page<ProjectDTO> findByOwnerIdWithPaging(int ownerId, Pageable pageable);
+    Page<ProjectDTO> findByOwnerIdOrderByUpdatedAtDescAndCreatedAtDesc(int ownerId, Pageable pageable);
     @Query("""
             SELECT new com.example.server.dto.ProjectDTO(p.id, p.name, p.description, p.status, p.createdAt, p.updatedAt)
             FROM Project p
-            JOIN p.owner o
-            WHERE p.id = :id AND o.id = :ownerId
+            WHERE p.id = :projectId AND p.owner.id = :ownerId
             """)
-    Optional<ProjectDTO> findByIdAndOwnerId(int id, int ownerId);
+    Optional<ProjectDTO> findByIdAndOwnerId(int projectId, int ownerId);
+    @Query("""
+            SELECT new com.example.server.dto.StatusCountDTO(p.status, COUNT(p))
+            FROM Project p
+            WHERE p.owner.id = :ownerId
+            GROUP BY p.status
+            """)
+    List<StatusCountDTO> countByStatusForOwner(int ownerId);
+    boolean existsByIdAndOwnerId(int projectId, int ownerId);
 }
